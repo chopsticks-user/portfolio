0e43e08a7ef4e00a8f76f2a9b71e26804cd1a5e7
chopsticks-user <frostyfrost273@gmail.com>
2026-02-02 13:20:48 -0500

    Scafford

--- eslint.config.js
+++ eslint.config.js
@@ -69,5 +69,5 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
     },
-  },
+  }
 );
--- src/routes/tools/index.tsx
+++ src/routes/tools/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <h1>Home</h1>;
+  return <h1>Tools</h1>;
 });
 
-export const head: DocumentHead = { title: "Home" };
+export const head: DocumentHead = { title: "Tools" };
